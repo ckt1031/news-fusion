776294f6d1357f46a29cf8587f1aa82deafb2810
ckt1031 <65409152+ckt1031@users.noreply.github.com>
2024-05-13 12:27:57 +0800

    Refactor parseRSS to handle multiple feed items correctly

--- tests/news.test.ts
+++ tests/news.test.ts
@@ -8,13 +8,8 @@ import { parseRSS } from '../src/lib/parse-news';
 
 test('Test News Availability', async () => {
 	for (const rssCategory of Object.keys(MUST_READ_RSS_LIST)) {
-		// Pick a random RSS from the list
-
-		// Have env.D1 means cloudflare worker, cloudfare worker has limited subrequest,
-		// so we need to pick a random rss to avoid hitting the limit
-
 		for (const rss of MUST_READ_RSS_LIST[rssCategory as RSS_CATEGORY]) {
-			expect(parseRSS(rss)).resolves.not.toBeNull();
+			expect(await parseRSS(rss)).not.toBeNull();
 		}
 	}
 });
