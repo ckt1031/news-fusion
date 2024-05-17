34bdca627cc3c7fffff8b615baa917fd2dd1415f
ckt1031 <65409152+ckt1031@users.noreply.github.com>
2024-05-17 14:53:38 +0800

    chore: Update discord-api-types npm dependency to version 0.37.84

--- src/lib/scrape.ts
+++ src/lib/scrape.ts
@@ -4,7 +4,7 @@ import type { ServerEnv } from '../types/env';
 /** Get website major content in markdown format from personal API */
 export async function scrapeToMarkdown(env: ServerEnv, url: string) {
 	const extractAPIResponse = await fetch(
-		`${env.TOOLS_API_BASE_URL}/web/extract/markdown?url=${url}`,
+		`${env.TOOLS_API_BASE_URL}/v1/web/extract/markdown?url=${url}`,
 		{
 			headers: {
 				accept: 'application/json',
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,6 @@
 import { sentry } from '@hono/sentry';
 import { Hono } from 'hono';
+import { HTTPException } from 'hono/http-exception';
 import discordBot from '../discord/bot';
 import { initSentry } from '../lib/sentry';
 import type { ServerEnv } from '../types/env';
@@ -12,13 +13,16 @@ app.get('/', (c) => {
 	return c.text('Hey Here!');
 });
 
-app.get('/cron', (c) => {
-	return c.text('Hey Here!');
-});
-
 app.route('/discord', discordBot);
 
 app.onError((e, c) => {
+	if (e instanceof HTTPException) {
+		return c.json(
+			{ error: e.getResponse().statusText || e.message || e.status },
+			e.status,
+		);
+	}
+
 	if (c.env.SENTRY_DSN && c.env.SENTRY_DSN.length > 0) {
 		const sentry = initSentry(c.env.SENTRY_DSN, c.executionCtx);
 		sentry.captureException(e);
