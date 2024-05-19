f1d9f6469af72445db16866e214f8362272c9265
ckt1031 <65409152+ckt1031@users.noreply.github.com>
2024-05-19 21:40:42 +0800

    Refactor translateText function to remove content length limit

--- src/config/news-sources.ts
+++ src/config/news-sources.ts
@@ -1,9 +1,15 @@
+import * as R from "remeda";
+
 export enum RSS_CATEGORY {
 	TECH = 'tech',
 	DEV = 'programming-dev',
 	AI = 'ai',
 	WEATHER = 'weather',
 	NATIONAL = 'national',
+
+	// General Blog with multiple categories
+	BLOG = 'blog',
+	APP = 'app',
 }
 
 export const AI_FILTER_RSS_LINKS: Partial<Record<RSS_CATEGORY, string[]>> = {
@@ -20,8 +26,6 @@ export const MUST_READ_RSS_LIST: Partial<Record<RSS_CATEGORY, string[]>> = {
 	[RSS_CATEGORY.TECH]: [
 		'https://vercel.com/atom',
 		'https://feeds.appinn.com/appinns',
-		'https://weekly.tw93.fun/rss.xml',
-		'https://feeds.feedburner.com/ruanyifeng',
 	],
 	[RSS_CATEGORY.DEV]: [
 		'https://bun.sh/rss.xml',
@@ -29,12 +33,28 @@ export const MUST_READ_RSS_LIST: Partial<Record<RSS_CATEGORY, string[]>> = {
 		'https://nodejs.org/en/feed/blog.xml',
 		'https://hellogithub.com/rss',
 		'https://blogs.windows.com/feed/',
+
+		// Developer Platform
+		'https://blog.replit.com/feed.xml',
 	],
+	[RSS_CATEGORY.APP]: ['https://discord.com/blog/rss.xml'],
 	[RSS_CATEGORY.AI]: ['https://huggingface.co/blog/feed.xml'],
 	[RSS_CATEGORY.WEATHER]: [
 		'https://rss.weather.gov.hk/rss/WeatherWarningBulletin_uc.xml',
 	],
+	[RSS_CATEGORY.BLOG]: [
+		// 'https://www.solidot.org/index.rss',
+
+		// Tinyfool
+		'https://codechina.org/feed/',
+		'https://weekly.tw93.fun/rss.xml',
+		'https://feeds.feedburner.com/ruanyifeng',
+		'https://www.williamlong.info/rss.xml',
+		'https://www.owenyoung.com/atom.xml',
+	],
 };
+
+export const ALL_RSS_LIST = R.mergeDeep(MUST_READ_RSS_LIST, AI_FILTER_RSS_LINKS);
 
 export const NEWS_MINIMALIST_API =
 	'https://www.newsminimalist.com/api/get-all-stories';
--- tests/news.test.ts
+++ tests/news.test.ts
@@ -1,13 +1,13 @@
 import { expect, test } from 'bun:test';
 import {
-	MUST_READ_RSS_LIST,
+	ALL_RSS_LIST,
 	type RSS_CATEGORY,
 } from '../src/config/news-sources';
 import { parseRSS } from '../src/lib/parse-news';
 
 test('Test News Availability', async () => {
-	for (const rssCategory of Object.keys(MUST_READ_RSS_LIST)) {
-		for (const rss of MUST_READ_RSS_LIST[rssCategory as RSS_CATEGORY] ?? []) {
+	for (const rssCategory of Object.keys(ALL_RSS_LIST)) {
+		for (const rss of ALL_RSS_LIST[rssCategory as RSS_CATEGORY] ?? []) {
 			expect(await parseRSS(rss)).not.toBeNull();
 		}
 	}
