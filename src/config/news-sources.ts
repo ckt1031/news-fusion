a6879d711394d5439e2fbe657c9459cc768c3126
ckt1031 <65409152+ckt1031@users.noreply.github.com>
2024-06-01 22:17:24 +0800

    Add checkImportance for alerts

--- src/config/news-sources.ts
+++ src/config/news-sources.ts
@@ -118,7 +118,7 @@ export const RSS_DEV: RssCatagory = {
 export const RSS_ALERTS: RssCatagory = {
 	name: RSS_CATEGORY.ALERTS,
 	discordChannelId: '1245007194064293929',
-	checkImportance: false,
+	checkImportance: true,
 	sources: ['https://rss.weather.gov.hk/rss/WeatherWarningBulletin.xml'],
 };
 
