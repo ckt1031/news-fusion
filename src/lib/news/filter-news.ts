cdd0ca679caa3a4c421d5ba7a671246af93c573c
ckt1031 <65409152+ckt1031@users.noreply.github.com>
2024-06-03 15:59:40 +0800

    refactor: Update news filter to use English keywords

--- src/lib/news/filter-news.ts
+++ src/lib/news/filter-news.ts
@@ -7,7 +7,7 @@ export default function filterRSS(prop: FilterProps) {
 	// If RSS has weather.gov.hk and title has 現時並無特別報告, skip
 	if (
 		prop.url.includes('weather.gov.hk') &&
-		prop.title.includes('現時並無特別報告')
+		prop.title.includes('no special')
 	) {
 		return false;
 	}
