89e561e26b34f14bcf88e539ec6182d15218bbfa
ckt1031 <65409152+ckt1031@users.noreply.github.com>
2024-06-09 12:35:31 +0800

    Modify some default config

--- src/config/api.ts
+++ src/config/api.ts
@@ -8,10 +8,10 @@ export const DEFAULT_EMBEDDING_MODEL = 'text-embedding-3-small';
 
 export const DEFAULT_SUMMARIZE_MODEL = 'gemini-1.5-flash-latest';
 
-export const DEFAULT_TRANSLATE_MODEL = 'yi-large';
+export const DEFAULT_TRANSLATE_MODEL = 'command-r-plus';
 
 export const DEFAULT_TITLE_GENERATE_MODEL = 'gpt-3.5-turbo-0125';
 
 export const DEFAULT_CHECK_IMPORTANCE_MODEL = 'gpt-4o';
 
-export const DEFAULT_MINIMUM_SIMILARITY_SCORE = 0.75;
+export const DEFAULT_MINIMUM_SIMILARITY_SCORE = 0.80;
