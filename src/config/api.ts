f8a97e098779c7a678f96ee9a8d76e9eeb9cb607
ckt1031 <65409152+ckt1031@users.noreply.github.com>
2024-06-09 21:53:42 +0800

    feat: use 0.75 for DEFAULT_MINIMUM_SIMILARITY_SCORE

--- src/config/api.ts
+++ src/config/api.ts
@@ -3,7 +3,7 @@ export const DISCORD_API_BASE = 'https://discord.com/api/v10';
 export const YOUTUBE_RSS =
 	'https://www.youtube.com/feeds/videos.xml?channel_id=';
 
-export const DEFAULT_MINIMUM_SIMILARITY_SCORE = 0.8;
+export const DEFAULT_MINIMUM_SIMILARITY_SCORE = 0.75;
 
 export const DEFAULT_EMBEDDING_MODEL = 'text-embedding-3-small';
 export const DEFAULT_SUMMARIZE_MODEL = 'gpt-3.5-turbo-0125';
