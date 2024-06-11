703bf8e9bd6897a49a2dbc0bb729b666e59cab78
ckt1031 <65409152+ckt1031@users.noreply.github.com>
2024-06-11 10:42:55 +0800

    Better env var calling for default model id

--- src/config/api.ts
+++ src/config/api.ts
@@ -6,8 +6,13 @@ export const YOUTUBE_RSS =
 export const DEFAULT_MINIMUM_SIMILARITY_SCORE = 0.75;
 
 export const DEFAULT_EMBEDDING_MODEL = 'text-embedding-3-small';
-export const DEFAULT_SUMMARIZE_MODEL = 'gpt-3.5-turbo-0125';
-export const DEFAULT_TRANSLATE_MODEL = 'gpt-4o';
-export const DEFAULT_TITLE_GENERATE_MODEL = 'gpt-3.5-turbo-0125';
-export const DEFAULT_CHECK_IMPORTANCE_MODEL = 'gpt-4o';
-export const DEFAULT_SEARCH_QUERY_GENERATE_MODEL = 'gpt-3.5-turbo-0125';
+export const DEFAULT_SUMMARIZE_MODEL =
+	process.env.DEFAULT_SUMMARIZE_MODEL ?? 'gpt-3.5-turbo-0125';
+export const DEFAULT_TRANSLATE_MODEL =
+	process.env.DEFAULT_TRANSLATE_MODEL ?? 'gpt-4o';
+export const DEFAULT_TITLE_GENERATE_MODEL =
+	process.env.DEFAULT_TITLE_GENERATE_MODEL ?? 'gpt-3.5-turbo-0125';
+export const DEFAULT_CHECK_IMPORTANCE_MODEL =
+	process.env.DEFAULT_CHECK_IMPORTANCE_MODEL ?? 'gpt-4o';
+export const DEFAULT_SEARCH_QUERY_GENERATE_MODEL =
+	process.env.DEFAULT_SEARCH_QUERY_GENERATE_MODEL ?? 'gpt-3.5-turbo-0125';
