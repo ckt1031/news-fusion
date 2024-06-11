fb400268ab92ab3a03e1f1648af6f7537c0210bc
ckt1031 <65409152+ckt1031@users.noreply.github.com>
2024-06-11 18:01:12 +0800

    chore: Replace `child_process` with `dax-sh`

--- scripts/generate-tool-api-schema.ts
+++ scripts/generate-tool-api-schema.ts
@@ -1,5 +1,4 @@
-import { exec } from 'node:child_process';
-import fs from 'node:fs';
+import $ from "dax-sh";
 
 const baseURL =
 	process.env.TOOLS_API_BASE_URL ?? 'https://tool-api.tsun1031.xyz';
@@ -7,10 +6,7 @@ const baseURL =
 const filePath = './src/types/tool-apis.d.ts';
 
 // Remove the existing schema file
-if (fs.existsSync(filePath)) {
-	fs.unlinkSync(filePath);
-	console.log(`${filePath} deleted successfully`);
-}
+await $`rm -f ${filePath}`;
 
 // Generate the OpenAPI schema for the tool APIs
-exec(`pnpm dlx openapi-typescript ${baseURL}/openapi.json -o ${filePath}`);
+await $`pnpm dlx openapi-typescript ${baseURL}/openapi.json -o ${filePath}`;
