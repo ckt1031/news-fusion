5e6a5d4d1ee20e7e578038acaa1db6ac0235b127
ckt1031 <65409152+ckt1031@users.noreply.github.com>
2024-06-22 16:36:55 +0800

    Add Next.js config, sitemap, and detailed robots handling
    - Introduced `next.config.js` to set reactStrictMode to false.
    - Replaced `robots.txt` with dynamic robots handling in `robots.ts`, specifying rules to disallow AI agents, setting crawl delay, and adding sitemap reference.
    - Created `sitemap.ts` to generate dynamic sitemap URLs and their properties based on topics.
    
    Enhances SEO and site accessibility management.

---
+++ src/app/robots.ts
@@ -0,0 +1,44 @@
+import type { MetadataRoute } from 'next';
+import { SITE_URL } from './sitemap';
+
+export default function robots(): MetadataRoute.Robots {
+	const ALL_AI_AGENTS = [
+		'ChatGPT-User',
+		'Applebot-Extended',
+		'Bytespider',
+		'CCBot',
+		'ClaudeBot',
+		'Diffbot',
+		'FacebookBot',
+		'Google-Extended',
+		'GPTBot',
+		'omgili',
+		'Amazonbot',
+		'Applebot',
+		'PerplexityBot',
+		'YouBot',
+		'anthropic-ai',
+		'Claude-Web',
+		'cohere-ai',
+	];
+
+	const ALL_AI_RULES: MetadataRoute.Robots['rules'] = ALL_AI_AGENTS.map(
+		(agent) => ({
+			userAgent: agent,
+			disallow: '/',
+		}),
+	);
+
+	return {
+		rules: [
+			...ALL_AI_RULES,
+			{
+				userAgent: '*',
+				crawlDelay: 10,
+				allow: '/',
+				disallow: ['/api', '/404', '/500', '/_next'],
+			},
+		],
+		sitemap: `${SITE_URL}/sitemap.xml`,
+	};
+}
