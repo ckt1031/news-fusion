82ac05601221d936d682cf121cf1b0b06bee5232
ckt1031 <65409152+ckt1031@users.noreply.github.com>
2024-06-28 22:55:48 +0800

    UI Enhancement

--- src/app/components/news/topic-selection.tsx
+++ src/app/components/news/topic-selection.tsx
@@ -3,6 +3,7 @@
 import { Button } from '@/app/components/ui/button';
 import { RSS_CATEGORY } from '@/config/news-sources';
 import { useRouter } from 'next/navigation';
+import { Combobox } from '../ui/combobox';
 
 interface Props {
 	topic: RSS_CATEGORY;
@@ -20,25 +21,49 @@ function captialFirstLetter(str: string) {
 export default function TopicSelection({ topic }: Props) {
 	const router = useRouter();
 
-	return (
-		<div className="flex flex-col">
-			<div className="flex flex-wrap py-2 gap-3">
-				{Object.values(RSS_CATEGORY).map((category) => (
-					<Button
-						key={category}
-						variant={category === topic ? 'secondary' : 'outline'}
-						className="py-0.5 px-3 rounded-lg"
-						onClick={() => {
-							const basePath =
-								category === RSS_CATEGORY.GENERAL ? '/' : `/topic/${category}`;
+	const catagoies = Object.values(RSS_CATEGORY);
 
-							router.push(`${basePath}${location.search}`);
-						}}
-					>
-						{captialFirstLetter(category)}
-					</Button>
-				))}
+	console.log(
+		catagoies.map((category) => ({
+			value: category,
+			label: captialFirstLetter(category),
+		})),
+	);
+
+	return (
+		<>
+			<div className="flex w-full justify-center items-center py-2 md:hidden">
+				<Combobox
+					value={topic}
+					setValue={(value) => router.push(`/topic/${value}`)}
+					values={catagoies.map((category) => ({
+						value: category,
+						label: captialFirstLetter(category),
+					}))}
+					placeholder="Select a topic"
+				/>
 			</div>
-		</div>
+			<div className="hidden md:block">
+				<div className="flex flex-wrap py-2 gap-3">
+					{catagoies.map((category) => (
+						<Button
+							key={category}
+							variant={category === topic ? 'secondary' : 'outline'}
+							className="py-0.5 px-3 rounded-lg"
+							onClick={() => {
+								const basePath =
+									category === RSS_CATEGORY.GENERAL
+										? '/'
+										: `/topic/${category}`;
+
+								router.push(`${basePath}${location.search}`);
+							}}
+						>
+							{captialFirstLetter(category)}
+						</Button>
+					))}
+				</div>
+			</div>
+		</>
 	);
 }
---
+++ src/app/components/ui/combobox.tsx
@@ -0,0 +1,81 @@
+'use client';
+
+import { Check, ChevronsUpDown } from 'lucide-react';
+import * as React from 'react';
+
+import { Button } from '@/app/components/ui/button';
+import {
+	Command,
+	CommandEmpty,
+	CommandGroup,
+	CommandInput,
+	CommandItem,
+	CommandList,
+} from '@/app/components/ui/command';
+import {
+	Popover,
+	PopoverContent,
+	PopoverTrigger,
+} from '@/app/components/ui/popover';
+import { cn } from '@/app/utils/cn';
+
+interface ComboboxProps {
+	value: string;
+	setValue: (value: string) => void;
+
+	values: { value: string; label: string }[];
+	placeholder: string;
+}
+
+export function Combobox({
+	value,
+	setValue,
+	values,
+	placeholder,
+}: ComboboxProps) {
+	const [open, setOpen] = React.useState(false);
+
+	return (
+		<Popover open={open} onOpenChange={setOpen}>
+			<PopoverTrigger asChild>
+				<Button
+					variant="outline"
+					role="combobox"
+					aria-expanded={open}
+					className="w-[250px] justify-between"
+				>
+					{value ? values.find((v) => v.value === value)?.label : placeholder}
+					<ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+				</Button>
+			</PopoverTrigger>
+			<PopoverContent className="w-[250px] p-0">
+				<Command>
+					<CommandInput placeholder="Search" />
+					<CommandEmpty>No value found.</CommandEmpty>
+					<CommandGroup>
+						<CommandList>
+							{values.map((d) => (
+								<CommandItem
+									key={d.value}
+									value={d.value}
+									onSelect={(currentValue: string) => {
+										setValue(currentValue === value ? '' : currentValue);
+										setOpen(false);
+									}}
+								>
+									<Check
+										className={cn(
+											'mr-2 h-4 w-4',
+											value === d.value ? 'opacity-100' : 'opacity-0',
+										)}
+									/>
+									{d.label}
+								</CommandItem>
+							))}
+						</CommandList>
+					</CommandGroup>
+				</Command>
+			</PopoverContent>
+		</Popover>
+	);
+}
