79692a5e753ee6183f91a5088dcc611ac26307ca
ckt1031 <65409152+ckt1031@users.noreply.github.com>
2024-06-30 16:06:47 +0800

    Reduce size by using Vercel KV and remove unsued deps

--- src/app/utils/upstash.ts
+++ src/app/utils/upstash.ts
@@ -1,7 +1,7 @@
-import { Redis } from '@upstash/redis';
+import { createClient } from '@vercel/kv';
 import { nextEnv } from '../env';
 
-export const redis = new Redis({
+export const redis = createClient({
 	url: nextEnv.UPSTASH_REDIS_REST_URL,
 	token: nextEnv.UPSTASH_REDIS_REST_TOKEN,
 });
