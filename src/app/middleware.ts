6f73ec645a46fe3654f01d5678c998e9fbb28430
ckt1031 <65409152+ckt1031@users.noreply.github.com>
2024-07-03 19:37:53 +0800

    Upstash rate-limit

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -1,7 +1,28 @@
+import { Ratelimit } from '@upstash/ratelimit';
 import type { NextRequest } from 'next/server';
 import { updateSession } from './utils/supabase/middleware';
+import { getMapCache, redis } from './utils/upstash';
+
+const ratelimit = new Ratelimit({
+	redis,
+	limiter: Ratelimit.slidingWindow(5, '10 s'),
+	analytics: true,
+	prefix: '@upstash/ratelimit',
+	ephemeralCache: getMapCache(),
+	enableProtection: true,
+});
 
 export async function middleware(request: NextRequest) {
+	const cfIp = request.headers.get('cf-connecting-ip');
+	const ip = cfIp ?? request.ip ?? '127.0.0.1';
+
+	// Check if the request is rate limited
+	const { success } = await ratelimit.limit(ip);
+
+	if (!success) {
+		return new Response('Rate limit exceeded', { status: 429 });
+	}
+
 	return await updateSession(request);
 }
 
--- src/app/utils/upstash.ts
+++ src/app/utils/upstash.ts
@@ -5,3 +5,14 @@ export const redis = createClient({
 	url: nextEnv.UPSTASH_REDIS_REST_URL,
 	token: nextEnv.UPSTASH_REDIS_REST_TOKEN,
 });
+
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+let cache: Map<string, any> | null = null;
+
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+export function getMapCache(): Map<string, any> {
+	if (!cache) {
+		cache = new Map();
+	}
+	return cache;
+}
