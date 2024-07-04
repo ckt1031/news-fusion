8ef4f871680e2b23b3587c7ca5d5f73b1b1c5df3
ckt1031 <65409152+ckt1031@users.noreply.github.com>
2024-07-04 20:46:15 +0800

    Bump deps and patch next.js config

--- next.config.js
+++ next.config.js
@@ -4,15 +4,18 @@ const withRoutes = nextRoutes({
 	outDir: 'src',
 });
 
+const isProd = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: {
-		remotePatterns: [
-			{
-				protocol: 'https',
-				hostname: '**',
-			},
-		],
+	reactStrictMode: isProd,
+	eslint: {
+		// Warning: This allows production builds to successfully complete even if
+		// your project has ESLint errors.
+		ignoreDuringBuilds: true,
+	},
+	experimental: {
+		reactCompiler: isProd,
 	},
 };
 
