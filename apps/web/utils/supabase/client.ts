d1615f46d7984f4e63fc388e4e61329019baf8f0
ckt1031 <65409152+ckt1031@users.noreply.github.com>
2024-07-21 21:36:38 +0800

    TSX fix for jobs app

---
+++ apps/web/utils/supabase/client.ts
@@ -0,0 +1,14 @@
+import { createBrowserClient } from '@supabase/ssr';
+import { nextClientEnv } from '../env/client';
+
+export function createSupabaseBrowserClient() {
+	return createBrowserClient(
+		nextClientEnv.NEXT_PUBLIC_SUPABASE_URL,
+		nextClientEnv.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+		{
+			cookieOptions: {
+				secure: process.env.NODE_ENV === 'production',
+			},
+		},
+	);
+}
---
+++ apps/web/utils/supabase/server.ts
@@ -0,0 +1,26 @@
+'use server';
+
+import { createServerClient } from '@supabase/ssr';
+import { cookies } from 'next/headers';
+import { nextServerEnv } from '../env/server';
+
+export async function createSupabaseServerClient() {
+	const cookieStore = cookies();
+
+	return createServerClient(
+		nextServerEnv.NEXT_PUBLIC_SUPABASE_URL,
+		nextServerEnv.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+		{
+			cookies: {
+				getAll() {
+					return cookieStore.getAll();
+				},
+				setAll(cookies) {
+					for (const { name, value, options } of cookies) {
+						cookieStore.set({ name, value, ...options });
+					}
+				},
+			},
+		},
+	);
+}
