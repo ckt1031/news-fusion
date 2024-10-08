89886056937eafc4a0c2b4c1f7e9412b9d70d95f
ckt1031 <65409152+ckt1031@users.noreply.github.com>
2024-10-08 10:17:15 +0800

    fix(expired): date comparison issue

--- remove_expired_db.py
+++ remove_expired_db.py
@@ -11,9 +11,11 @@ init_logger()
 
 
 def remove_expired_articles():
+    exceed_date = datetime.datetime.now() - datetime.timedelta(days=expiration_days)
     Article.delete().where(
-        Article.created_at
-        < (datetime.datetime.now() - datetime.timedelta(days=expiration_days))
+        Article.created_at.day <= exceed_date.day
+        and Article.created_at.month <= exceed_date.month
+        and Article.created_at.year <= exceed_date.year
     ).execute()
 
     logger.success("Expired articles removed")
