5996e029fab900ebd52e677286e9360f43a99e6a
ckt1031 <65409152+ckt1031@users.noreply.github.com>
2025-01-09 18:05:05 +0800

    refactor(forum_links_importance.py): improve prompt instructions and add more specific examples to allowed and disallowed lists

--- lib/prompts/forum_links_importance.py
+++ lib/prompts/forum_links_importance.py
@@ -1,5 +1,5 @@
 allowed = """
-- Topics: General topics include but not limited to: tech, science, programming, or business (Apply to all below criteria)
+- General topics include but not limited to: tech, science, programming, or business (Apply to all below criteria)
 - Significant tech innovations, or breakthroughs
 - Personal review or thoughts about stuffs
   - e.g. Why I'm over React
@@ -17,36 +17,37 @@ allowed = """
   - Specific (detailed) tech on 
   - New version, important or useful updates
 - Useful skills
+- Product problems, or solutions
 - Side projects/Habits that worth sharing, or worth reading
 - Their successful stories, experiences or their failure stories
 """.strip()
 
 disallowed = """
+- Promotions, or advertisements of community, groups like Telegram, Discord, or any other social media
 - Any unwanted, unuseful or unworthy content
 - Negative, harmful or blaming content
 - Stupid questions, or questions that are not worth asking
-- Funding:
-  - e.g. We raised $1M for our new project
+- Technical issues, bugs, like: cannot find the solution, or asking for help, cannot access, or cannot login issues
 - Job opportunities, job search, or job-seeking advice
 - My favourite, or my best, or my worst [Music, Movie, etc.] (Leave book reviews as they are useful)
 """.strip()
 
 forum_importance_prompt = f"""
-You are designed to browse and find important discussions that are worth reading for global, tech innovations, or developers.
-Help users save time by identifying whether an article is worth reading for extra knowledge and personal improvement.
+You are designed to browse and find important discussions that are worth reading for global, economy, future, tech innovations, or developers.
+Help users save time by identifying whether an article is worth reading for news, extra knowledge and personal improvement.
 Evaluate the importance of the discussions.
 
-## Instructions
+# Instructions
 
 - Mark as important (true) based on "Important Factors" sections.
 - If content satisfies "Not Important Factors" sections, mark as not important (false).
 - If the article is significantly impactful, set "important" to true, no matter they includes content in "Not Important Factors" section.
 
-## Important Factors (true)
+# Important Factors (true)
 
 {allowed}
 
-### Not Important Factors (false)
+# Not Important Factors (false)
 
 {disallowed}
 """.strip()
