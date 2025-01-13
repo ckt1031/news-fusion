47438ea9435132feae021d015ece532bb61506d7
ckt1031 <65409152+ckt1031@users.noreply.github.com>
2025-01-13 12:02:53 +0800

    feat(server.py): add openapi schema configuration to the app feat(server.py): add server url configuration to openapi schema feat(server.py): add description to openapi schema

--- server.py
+++ server.py
@@ -3,12 +3,14 @@ from collections.abc import AsyncIterator
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.openapi.utils import get_openapi
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
 from fastapi_limiter import FastAPILimiter
 
 import web.routes as v1_router
 from lib.db.redis_client import redis
+from lib.env import get_env
 from lib.pubsub.subscription import register_all_topics
 from lib.scheduler import scheduler
 from lib.utils import init_logger
@@ -33,7 +35,34 @@ async def lifespan(_: FastAPI) -> AsyncIterator[None]:
 app = FastAPI(
     title="News Fusion",
     lifespan=lifespan,
+    servers=[
+        {
+            "url": get_env("SERVER_URL", "http://localhost:4782"),
+        }
+    ],
 )
+
+
+def openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="News Fusion",
+        version="0.1.0",
+        description="This is the API documentation for News Fusion",
+        openapi_version="3.0.0",  # Use OpenAPI 3.0 for compatibility with other tools
+        servers=[
+            {
+                "url": get_env("SERVER_URL", "http://localhost:4782"),
+            }
+        ],
+        routes=app.routes,
+    )
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+app.openapi = openapi
 
 
 # Health check
