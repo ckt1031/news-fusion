7070e4ee5473de1bd8711bff0aec96d78007b911
ckt1031 <65409152+ckt1031@users.noreply.github.com>
2025-06-05 01:30:39 +0800

    feat: enhance article processing with new scraping and similarity features, update RSS feed configuration

--- crawler/index.ts
+++ crawler/index.ts
@@ -1,21 +1,30 @@
-import { RSS_CATEGORIES, type RSSFeed } from '../config/sources';
+import { RSS_CATEGORIES, type RSSConfigFeed } from '../config/sources';
 import { parseRSS } from '../lib/rss';
+import Similarity from '../lib/similarity';
 import { handleEntry } from './entry';
 
-async function handleFeed(feed: RSSFeed) {
+async function handleFeed(feed: RSSConfigFeed) {
 	console.log(`Handling feed: ${feed.name}`);
 
 	const parsedFeed = await parseRSS(feed.url, 24);
 
 	for (const entry of parsedFeed.items) {
-		handleEntry(entry);
+		await handleEntry({
+			feedConfig: feed,
+			feedData: entry,
+		});
 	}
 }
 
 async function runScraper() {
+	const similarity = new Similarity();
+
+	// Initialize the similarity collection.
+	await similarity.initializeCollection();
+
 	for (const category of RSS_CATEGORIES) {
 		// Handle each feed in the category.
-		for (const feed of category.feeds) handleFeed(feed);
+		for (const feed of category.feeds) await handleFeed(feed);
 	}
 }
 
