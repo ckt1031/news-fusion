8f728f6d38f119cb6e22849c7749da96c806180d
ckt1031 <65409152+ckt1031@users.noreply.github.com>
2025-06-05 21:30:01 +0800

    feat: add CORS, ETag, and secure headers middleware to API for enhanced security and performance

--- api/index.ts
+++ api/index.ts
@@ -2,8 +2,15 @@ import { handle } from '@hono/node-server/vercel';
 import { Hono } from 'hono';
 import { router as cronRouter } from './cron.js';
 import { router as feedRouter } from './feed.js';
+import { cors } from 'hono/cors';
+import { etag } from 'hono/etag';
+import { secureHeaders } from 'hono/secure-headers';
 
 const app = new Hono();
+
+app.use('*', cors());
+app.use('*', etag());
+app.use('*', secureHeaders());
 
 app.route('/v1/feeds', feedRouter);
 app.route('/v1/cron', cronRouter);
