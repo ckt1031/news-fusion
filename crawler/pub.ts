6860314d25230ea80908f8ee3b52c1dbb2d96df4
ckt1031 <65409152+ckt1031@users.noreply.github.com>
2025-06-06 14:46:25 +0800

    refactor: modify sendPub function to use structured headers and body for axios post request

--- crawler/pub.ts
+++ crawler/pub.ts
@@ -4,10 +4,16 @@ export async function sendPub(category: string) {
 	const SITE_DOMAIN = process.env.SITE_DOMAIN || 'localhost:3000';
 	const PUBSUB_URL = 'https://pubsubhubbub.appspot.com/';
 
-	await axios.post(PUBSUB_URL, {
+	const headers = {
+		'Content-Type': 'application/x-www-form-urlencoded',
+	};
+
+	const body = {
 		'hub.mode': 'publish',
 		'hub.url': `https://${SITE_DOMAIN}/v1/feeds/${category}.xml`,
-	});
+	};
+
+	await axios.post(PUBSUB_URL, body, { headers });
 }
 
 sendPub('business');
